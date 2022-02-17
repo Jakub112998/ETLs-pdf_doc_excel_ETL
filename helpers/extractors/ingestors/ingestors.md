75ebda4acc1b9ab1b7e39e29e81e423754ff75ea
James Koledo <jakubcydejko7@gmail.com>
2022-02-18 00:21:18 +0100

    handlers added

--- helpers/extractors/ingestors/ingestors.md
+++
@@ -1,5 +0,0 @@
-Ingestors mają za zadanie sprowadzić pliki do readable formatu.
-najważniejsze są ingestors do plików:
-- pdf
-- jpg
-- doc
--- helpers/extractors/pdf_extractor.py
+++ helpers/extractors/pdf_extractor.py
@@ -1,26 +1,97 @@
-class PDFHandler(FileHandler):
+import logging
+import datetime
+import os
+from .. import factory
+import pytesseract
+import shutil
+import tabula
+
+
+class PDFExtractor():
     def __init__(self, destination):
         self._destination = destination
+        pass
 
-    def process(self, input):
-        # read PDF file
-        # uncomment if you want to pass pdf file from command line arguments
-        tables = tabula.read_pdf(input.split("/")[1], pages="all")
-        print(tables)
-        # save them in a folder
-        folder_name = self._destination.split("/")[1]
-        if not os.path.isdir(folder_name):
-            os.mkdir(folder_name)
+    def _extract_text(self):
+        # open the PDF file - extract text
+        PDFfile = open(
+            '/task/data/in/3_formularz_cenowy_pak16_8d2731ef55bad25ee24cb9add1eaf148c20bf0f6062db40652b50f8ce8d6c331.pdf',
+            'rb')
+        PDFfilereader = PyPDF2.PdfFileReader(PDFfile)
+        # print(PDFfilereader.getDocumentInfo())
+        f = PDFfilereader.getFields()
+        # print(f.values())
+        for value in f.values():
+            print(type(value))
+            for v in value.values():
+                try:
+                    print(v['/Contents'].rstrip('\x00'))
+                    # print(v['/Contents'].decode('utf-8'))
+                except Exception as e:
+                    print(e)
+                # print(v['/Contents'])
+                # for z in v.values():
+                #     print(z)
+                # for y in z.values():
+                #     print(y)
+                # print(z['/Contents'])
+        # print(PDFfilereader.getXmpMetadata())
+        # print the number of pages
+        # print(PDFfilereader.numPages)
+        # provide the page number
+        pages = PDFfilereader.getPage(1)
+        # extracting the text in PDF file
+        # print(pages.extractText())
+        # close the PDF file
+        PDFfile.close()
+
+    def _extract_tables(self):
+        # reading both table as an independent table
+        tables = tabula.read_pdf(file, pages=1, multiple_tables=True)
+        print(tables[0])
+        print(tables[1])
         # iterate over extracted tables and export as excel individually
         for i, table in enumerate(tables, start=1):
             table.to_excel(os.path.join(folder_name, f"table_{i}.xlsx"), index=False)
 
-        # convert all tables of a PDF file into a single CSV file
-        # supported output_formats are "csv", "json" or "tsv"
-        tabula.convert_into(input.split("/")[1], "output.csv", output_format="csv", pages="all")
-        # convert all PDFs in a folder into CSV format
-        # `pdfs` folder should exist in the current directory
-        tabula.convert_into_by_batch("pdfs", output_format="csv", pages="all")
+    def _extract_images(self):
+        # open the fitz file
+        pdf = fitz.open(file)
+        # select the page number
+        image_list = pdf.getPageImageList(0)
+        # applying the loop
+        for image in image_list:
+            xref = image[0]
+            pix = fitz.Pixmap(pdf, xref)
+            if pix.n < 5:
+                pix.writePNG(f'{xref}.png')
+            else:
+                pix1 = fitz.open(fitz.csRGB, pix)
+                pix1.writePNG(f'{xref}.png')
+                pix1 = None
+            pix = None
+        # print the images
+        print(len(image_list), 'detected')
+
+    def process(self):
+        print("PDF processing")
+
+def main(logger, target_path, base_url, start_page, end_page):
+    industry = target_path.split("/")
+    industry = industry[len(industry) - 2]
+    data_source = industry[len(industry) - 1]
+
+    # https://selenium-python.readthedocs.io/locating-elements.html
+
+    with PDFExtractor(destination=target_path) as handler:
+        handler.process()
+        # driver = handler.get_driver()
+        # print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        # for page_number in range(start_page, end_page):  # zaczynać od 1
+        #     try:
+        #         logger.debug(f"{page_number} page of {industry} industry from {data_source} was scraped")
+        #     except Exception as e:
+        #         print(e)
 
 
 class BizfindBot:  # ten obiekt jest tworzony przy ładowaniu pluginów
@@ -51,23 +122,13 @@ class BizfindBot:  # ten obiekt jest tworzony przy ładowaniu pluginów
         main(logger=myLogger, target_path=target_path, base_url=self.url, start_page=start_page, end_page=end_page)
 
         logging.shutdown()
-        # -------------------------------------------------------------------------
-        # login = driver.find_element_by_name('UserName').send_keys("name")
-        # pwd = driver.find_element_by_name('Password').send_keys("password")
-        # submit = driver.find_element_by_class_name('button').click()
-
-        # myLogger.setLevel(logging.INFO)
-        # myLogger.debug("debug")
-        # myLogger.info("informuje")
-        # myLogger.warning("ostrzeżenie")
-        # myLogger.error("error")
-        # myLogger.critical("critical")
 
 
-# def register() -> None:
-#     factory.register("bizfind", BizfindBot)
+def register() -> None:
+    factory.register("bizfind", BizfindBot)
 
 
-if __name__ == "__main__":
-    ob = BizfindBot()
-    ob.scrape()
+# if __name__ == "__main__":
+    # ob = BizfindBot()
+    # ob.scrape()
+    # detect_tekst()
